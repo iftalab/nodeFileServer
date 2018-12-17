f0c616b9b6aed21a555ca0fd719352f80bf8bde3
adorgolap <adorgolap@gmail.com>
2018-12-17 22:37:26 +0600

    db is pointed to smart tech

--- dbHelper.js
+++ dbHelper.js
@@ -1,31 +1,14 @@
-exports.dbConnection = function () {
-    var mysql = require('mysql');
-
-    var fs = require('fs');
-    const config = JSON.parse(fs.readFileSync("dbConfig.json"));
-    global.host = config.host;  //global variable(overwritten)
-    global.port = config.port;  //global variable(overwritten)
-    var connection = mysql.createConnection({
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database,
-        multipleStatements: config.multipleStatements
-    });
-
-    connection.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
-    return connection;
-}
 exports.makeMongoDBConnection = function(){
     var mongoose = require('mongoose');
-    var fs = require('fs');
-    const config = JSON.parse(fs.readFileSync("dbConfig.json"));
+    // var fs = require('fs');
+    // const config = JSON.parse(fs.readFileSync("dbConfig.json"));
+    // mongoose.connect(
+    //     "mongodb+srv://"+config.mongoUser+":"+config.mongoPass
+    //     + "@icluster-txko2.mongodb.net/test?retryWrites=true")
+    //     .then()
+    //     .catch();
     mongoose.connect(
-        "mongodb+srv://"+config.mongoUser+":"+config.mongoPass
-        + "@icluster-txko2.mongodb.net/test?retryWrites=true")
+        "mongodb://ifta:bridge1599@27.147.128.14:27017/appDb?retryWrites=true")
         .then()
         .catch();
 }
